bbadd628adcc8d77490cff062cddaa350201e7b5
pvhumphr <pvhumphr@usc.edu>
2018-07-15 06:42:21 -0700

    Version 0.2 with sample.

--- app.py
+++ app.py
@@ -17,49 +17,86 @@ from flask import (
     redirect)
 from flask_cors import CORS
 import os.path
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 
 # Assigning the Flask framework.
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def home():
     print(request.headers)
     return render_template("index.html")
 
-@app.route('/sample', methods=['GET'])
+
+@app.route('/api/sample', methods=['GET'])
 def sample():
 
     if request.method == 'GET':
 
-        try:
-            # Create json dictionary to hold the data.
-            json_dict = {}
-            json_dict['Sample'] = "sample"
+        # Load the diabetes dataset
+        diabetes = datasets.load_diabetes()
 
-            json_object = jsonify(json_dict)
-            print("jsonify Okay")
+        # Use only one feature
+        diabetes_X = diabetes.data[:, np.newaxis, 2]
 
-            response = json_object
-            response.headers['Content-Type'] = "application/json"
+        # Split the data into training/testing sets
+        diabetes_X_train = diabetes_X[:-20]
+        diabetes_X_test = diabetes_X[-20:]
 
-            return response
+        # Split the targets into training/testing sets
+        diabetes_y_train = diabetes.target[:-20]
+        diabetes_y_test = diabetes.target[-20:]
 
-        except Exception as e:
-            print('/sample failed', str(e))
-            return str(e)
+        # Create linear regression object
+        regr = linear_model.LinearRegression()
+
+        # Train the model using the training sets
+        regr.fit(diabetes_X_train, diabetes_y_train)
+
+        # Make predictions using the testing set
+        diabetes_y_pred = regr.predict(diabetes_X_test)
+
+        # Return test and predictions.
+        json_dict = {}
+        X_list = []
+        Xs = diabetes_X_test.tolist()
+    
+        for X in Xs:
+            X_list.append(X[0])
+
+      
+
+        json_dict['diabetes_X_test'] = X_list
+        json_dict['diabetes_y_test'] = diabetes_y_test.tolist()
+        json_dict['diabetes_y_pred'] = diabetes_y_pred.tolist()
+        print(json_dict)
+
+        json_object = jsonify(json_dict)
+        print("jsonify Okay")
+
+        response = json_object
+        response.headers['Content-Type'] = "application/json"
+
+        print(response)
+
+        return response
+
 
 if __name__ == "__main__":
     hostname = socket.gethostname()
     print("socket.hostname():", hostname)
-    
+
     if (hostname == 'XPS'):
         app.run(debug=True)
-    elif (hostname == 'DESKTOP-S08TN4O'):  
+    elif (hostname == 'DESKTOP-S08TN4O'):
         app.run(debug=True)
     else:
         from os import environ
         print("Port", environ.get("PORT", "Not Found"))
-        app.run(debug=False, host='0.0.0.0', port=int(environ.get("PORT", 5000)))
-
+        app.run(debug=False, host='0.0.0.0',
+                port=int(environ.get("PORT", 5000)))
---
+++ static/js/sample.js
@@ -0,0 +1,68 @@
+console.log("sample.js was constructed.")
+
+try {
+    Plotly.purge('sample_chart');
+
+    console.log("\n\n Purged sample_chart.")
+} catch (err) {
+    console.log("\n\nPurge", err.message)
+}
+
+
+var window_location_host = window.location.host
+
+if (window_location_host.indexOf("heroku") == -1) {
+    url = "http://"
+}
+else {
+    url = "https://"
+}
+
+
+url = url + window_location_host + "/api/sample"
+console.log("\n\nurl:", url)
+
+try {
+    d3.json(url, function (error, response) {
+
+        if (error) {
+            console.log("\n\n")
+            console.log("****************")
+            console.log("d3.json failed.")
+            return console.warn(error);
+        }
+
+        console.log("\n\nresponse:")
+        console.log(response)
+
+        var trace1 = {
+            x: response['diabetes_X_test'],
+            y: response['diabetes_y_test'],
+            mode: 'markers',
+            name: 'test'
+        };
+
+        var trace2 = {
+            x: response['diabetes_X_test'],
+            y: response['diabetes_y_pred'],
+            mode: 'lines',
+            name: 'prediction'
+        };
+
+        var data = [trace1, trace2];
+
+        var layout = {
+            showlegend: false,
+            xaxis: { title: 'Data', range: [0, 0.1] },
+            yaxis: { title: 'Observation' },
+            autosize: false,
+            height: 500,
+            width: 900
+        };
+
+        Plotly.newPlot('sample_chart', data, layout)
+    }
+)
+} catch (err) {
+    console.log(err.message)
+}
